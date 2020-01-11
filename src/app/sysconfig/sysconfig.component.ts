6810a71d6d3e2e7c8c415b2eb6c347178ad5e917
vilas <ethan@abc.com>
2020-01-11 18:42:16 +0800

    202-01-11

--- src/app/sysconfig/sysconfig.component.ts
+++ src/app/sysconfig/sysconfig.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'sysconfig',
   templateUrl: './sysconfig.component.html',
-  styleUrls: ['./sysconfig.component.scss']
+  styleUrls: ['./sysconfig.component.scss'],
+  moduleId: module.id
 })
 export class SysconfigComponent implements OnInit {
 
